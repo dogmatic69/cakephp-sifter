2ba3bcd869a44d1624e1b9c3d793cd1406d84ded
dogmatic69 <dogmatic69@gmail.com>
2014-02-14 00:40:53 +0000

    adding frontend for rendering search forms

---
+++ View/Elements/sift.ctp
@@ -0,0 +1,38 @@
+<?php
+$model = $this->Sifter->getModel();;
+$fields = $this->Sifter->getFields();
+
+echo $this->Form->create(null, array(
+	'class' => 'sifter-form form-inline',
+	'data-ajax-source' => Router::url(array('ext' => 'json')),
+	'data-ajax-min-length' => 2,
+	'inputDefaults' => array(
+		'div' => 'form-group',
+		'class' => 'form-control',
+	)
+));
+	echo $this->Form->hidden('Sifter.location', array('value' => $this->request->here));
+	echo $this->Form->hidden('Sifter.search_field');
+	echo $this->Form->hidden('Sifter.search_value');
+	foreach ($fields as $options) {
+		$options['data-field'] = $options['field'];
+		unset($options['field']);
+
+		if ($options['type'] == 'text' && !empty($options['class']) && $options['class'] == 'datetime') {
+			$start = $end = array_merge($options, array('div' => false, 'label' => false));
+			$start['class'] .= ' start';
+			$end['class'] .= ' end';
+
+			$start['placeholder'] = __d('sifter', 'Start date');
+			$end['placeholder'] = __d('sifter', 'End date');
+			echo $this->Html->tag('div', implode('', array(
+				$this->Form->label($options['data-field']),
+				$this->Form->input($options['data-field'] . '_start', $start),
+				$this->Form->input($options['data-field'] . '_end', $end)
+			)), array('class' => 'form-group'));
+		} else {
+			echo $this->Form->input($options['data-field'], $options);
+		}
+	}
+	echo $this->Form->submit(__d('sifter', 'Sift records'));
+echo $this->Form->end();
---
+++ View/Helper/SifterHelper.php
@@ -0,0 +1,95 @@
+<?php
+App::uses('AppHelper', 'View/Helper');
+
+class SifterHelper extends AppHelper {
+
+	public $helpers = array(
+		'Html',
+	);
+/**
+ * Get the default model name
+ *
+ * @var string
+ */
+	public $defaultModel = null;
+
+	public function beforeLayout($layoutFile) {
+		parent::beforeLayout($layoutFile);
+		
+		$this->Html->script('Sifter.sifter', array(
+			'inline' => false,
+		));
+	}
+
+/**
+ * Default Constructor
+ *
+ * @param View $View The View this helper is being attached to.
+ * @param array $settings Configuration settings for the helper.
+ */
+	public function __construct(View $View, $settings = array()) {
+		parent::__construct($View, $settings);
+		$this->_defaultModel();
+	}
+
+/**
+ * Get the name of the model being used
+ *
+ * @return string
+ */
+	public function getModel() {
+		return $this->defaultModel;
+	}
+	
+/**
+ * Get the form fields
+ *
+ * @param string $model the model name to use (Plugin.Name format)
+ *
+ * @return array
+ */
+	public function getFields($model = null) {
+		$fields = Hash::extract(self::_Model($model)->sifterConfig('fields'), '{s}.input');
+		foreach ($fields as &$config) {
+			if (isset($config['options']['className'])) {
+				unset($config['options']);
+			}
+		}
+		return $fields;
+	}
+
+/**
+ * Get a model instance
+ *
+ * @param string $model the name of the model to use
+ *
+ * @return Model
+ */
+	protected function _Model($model) {
+		return ClassRegistry::init($model ? $model : $this->defaultModel);
+	}
+
+/**
+ * Figure out the model name and save the value
+ *
+ * @param string $model the name of the model (optional, blank for autodetect)
+ *
+ * @return string
+ */
+	protected function _defaultModel($model = null) {
+		if (!empty($model)) {
+			$this->defaultModel = $model;
+			return $model;
+		}
+
+		if (!empty($this->request->params['models'])) {
+			$model = implode('.', $this->request->params['models'][key($this->request->params['models'])]);
+		}
+
+		if (empty($model)) {
+			pr($this);
+		}
+		$this->defaultModel = $model;
+		return $model;
+	}
+}
